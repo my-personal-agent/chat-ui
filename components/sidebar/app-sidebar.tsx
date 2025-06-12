6201c482f14f08ec00728b633cad5b8f91dec11c
Wai Yan Min Khaing <waiyanminkhaing1997@gmail.com>
2025-06-12 09:44:46 +0900

    add app sidebar

---
+++ components/sidebar/app-sidebar.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { NavDocuments } from "@/components/sidebar/nav-chats";
+import { NavMain } from "@/components/sidebar/nav-main";
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { IconInnerShadowTop } from "@tabler/icons-react";
+import * as React from "react";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  documents: [
+    {
+      name: "Data Library",
+      url: "#",
+    },
+    {
+      name: "Reports",
+      url: "#",
+    },
+    {
+      name: "Word Assistant",
+      url: "#",
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">My Personal AI</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain />
+        <NavDocuments items={data.documents} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
